d5f5f61e7011637119474004e48d293062c9b8b6
nielsjani <niels.jani@cegeka.be>
2018-02-11 22:05:56 +0100

    NielsJ: code for basic authentication

--- src/main/java/com/cegeka/switchfully/security/SecurityConfig.java
+++ src/main/java/com/cegeka/switchfully/security/SecurityConfig.java
@@ -1,7 +1,31 @@
 package com.cegeka.switchfully.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.AuthenticationEntryPoint;
 
 @Configuration
-public class SecurityConfig {
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private AuthenticationEntryPoint authEntryPoint;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests()
+                .anyRequest().authenticated()
+                .and().httpBasic()
+                .authenticationEntryPoint(authEntryPoint);
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("JMILLER").password("THANKS").roles("BLANK");
+    }
+
 }
