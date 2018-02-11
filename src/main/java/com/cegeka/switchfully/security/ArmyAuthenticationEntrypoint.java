9502d90786ea37fa9177dcbe10bb0a6537b9b202
nielsjani <niels.jani@cegeka.be>
2018-02-11 22:05:56 +0100

    NielsJ: code for basic authentication

---
+++ src/main/java/com/cegeka/switchfully/security/ArmyAuthenticationEntrypoint.java
@@ -0,0 +1,28 @@
+package com.cegeka.switchfully.security;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@Component
+public class ArmyAuthenticationEntrypoint extends BasicAuthenticationEntryPoint {
+
+    public ArmyAuthenticationEntrypoint() {
+        setRealmName("ARMYRealm");
+    }
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authEx)
+            throws IOException, ServletException {
+        response.addHeader("WWW-Authenticate", "Basic realm=" +getRealmName());
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        PrintWriter writer = response.getWriter();
+        writer.println("HTTP Status 401 - " + authEx.getMessage());
+    }
+}
