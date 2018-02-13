8981e9e928bd8060907a609cc166c63aec117638
nielsjani <niels.jani@cegeka.be>
2018-02-13 21:25:20 +0100

    NielsJ: add unit test

---
+++ src/test/java/com/cegeka/switchfully/security/ArmyAuthenticationEntrypointTest.java
@@ -0,0 +1,35 @@
+package com.cegeka.switchfully.security;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ArmyAuthenticationEntrypointTest {
+
+    @InjectMocks
+    private ArmyAuthenticationEntrypoint entrypoint;
+
+    @Test
+    public void commence_shouldSetUnauthorisedStatusAndHeader() throws IOException, ServletException {
+
+        HttpServletRequest request = new MockHttpServletRequest();
+        HttpServletResponse response = new MockHttpServletResponse();
+
+        entrypoint.commence(request, response, null);
+
+        assertThat(response.getHeader("WWW-Authenticate")).isEqualTo("Basic realm=ARMYRealm");
+        assertThat(response.getStatus()).isEqualTo(401);
+    }
+}
